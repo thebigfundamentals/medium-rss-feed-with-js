c008c5c5f0e88c7e8c55da72466a535e544aa290
gsc <gabrielschincariol@gmail.com>
2021-08-06 12:00:27 -0300

    Added: beta carousel

---
+++ carousel/app.js
@@ -0,0 +1,56 @@
+const username = `gschincariol`
+const RSSUrl = `https://medium.com/feed/@${username}`;
+const RSSConverter = `https://api.rss2json.com/v1/api.json?rss_url=${RSSUrl}`;
+
+const $author = document.querySelector('title');
+const $carousel = document.querySelector('.mediumCarousel');
+
+const getMediumData = async () => {
+    
+    try {
+    const response = await fetch(RSSConverter);
+    const data = await response.json();
+    console.log(data);
+    return data
+    } catch(error){
+        console.log(error)
+    }
+};
+
+const getAuthor = async () => {
+    const data = await getMediumData();
+    const author = data.feed.title;
+    $author.textContent = author;
+};
+
+const makeCarousel = async () => {
+    const data = await getMediumData();
+    const posts = data.items;
+
+    for (let i = 0; i < posts.length; i++){
+        const image = posts[i].thumbnail;
+        const title = posts[i].title.toUpperCase();
+        const pubDate = posts[i].pubDate;
+        const carouselItem = document.createElement('div');
+        const link = posts[i].link;
+        const isActive = () => {
+            return (i === 0 ? "active" : "");
+        }
+
+        carouselItem.className = `carousel-item ${isActive()}`;
+        carouselItem.innerHTML = `
+        <img src="${image}" class="d-block mediumImg img-fluid" alt="">
+        <div class="carousel-caption d-none d-md-block">
+          <h5 class="bg-dark bg-opacity-50 w-auto"><a class="text-warning" href="${link}">${title}</a></h5>
+          <p class="text-muted">Published: ${pubDate}</p>
+        </div>`;
+
+        $carousel.appendChild(carouselItem);
+    }
+
+
+};
+
+getAuthor();
+makeCarousel();
+
